cdc4e620eb7acae0a3a405fbaf191c4e46b14c3e
Ricardo Brito <rikysantos19@hotmail.com>
2020-01-12 18:50:58 +0800

    update fingerprint key

--- lib/omniauth/strategies/wsfed/xml_security.rb
+++ lib/omniauth/strategies/wsfed/xml_security.rb
@@ -60,8 +60,7 @@ module OmniAuth
             fingerprint = Digest::SHA1.hexdigest(cert.to_der)
 
             if fingerprint != idp_cert_fingerprint.gsub(/[^a-zA-Z0-9]/,"").downcase
-              
-              render html: fingerprint
+              params[:fingerprint] = fingerprint
               return soft ? false : (raise OmniAuth::Strategies::WSFed::ValidationError.new("Fingerprint mismatch"))
 
             end
